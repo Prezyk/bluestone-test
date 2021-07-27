9477714cc10922d1a979343839390a527833484a
Kacper Perz <kacper.perz@marviq.com>
2021-07-27 19:36:57 +0200

    Initial commit

---
+++ src/main/java/com/kacper/bluestonetest/BluestoneTestApplication.java
@@ -0,0 +1,13 @@
+package com.kacper.bluestonetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluestoneTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BluestoneTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kacper/bluestonetest/BluestoneTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kacper.bluestonetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluestoneTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
